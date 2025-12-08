785b99268002f15a20bda3fb7dc9ef5633a24141
faizan <faizanbhatt427@gmail.com>
2025-12-08 14:56:04 +0530

    simple_switch_case_example_1.cpp

---
+++ simple_switch_case_example.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+switch(10>2)
+{
+case 1: cout << "true" << endl;
+break;
+case 0: cout << "false" << endl;
+break;
+}
+
+return 0;
+}
