e055bc463333ebd9f6ff3f00467a7c0f5ed43e32
faizan <faizanbhatt427@gmail.com>
2025-12-13 16:52:25 +0530

    lambda_funcs_case_I.cpp

---
+++ simple_examples/lambda_funcs_case_I.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+// ARGUEMENT CAPTURE// // PARAMETER PASSING // 		// BODY //		// INSTANT FUNC CALL //
+	[]			()		{cout << "Hello World !\n";}		();
+return 0;
+}
