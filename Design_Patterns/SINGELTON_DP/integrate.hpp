a4171031eef69d1c1aeac3c599c5787df5ad80fc
faizan <faizanbhatt427@gmail.com>
2025-12-24 16:56:56 +0530

    integrate.hpp

---
+++ Design_Patterns/SINGELTON_DP/integrate.hpp
@@ -0,0 +1,17 @@
+#ifndef INTEGRATE_HPP
+#define INTEGRATE_HPP
+#include <mutex>
+#include <iostream>
+#include <thread>
+using namespace std;
+class SINGELTON
+{
+private:
+	static SINGELTON *ptr;
+	static mutex mtx;
+	SINGELTON();
+public:
+	static * create_instance();
+	
+};
+#endif
