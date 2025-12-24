2e0dca37937f079d38e941cd0b270a3758661653
faizan <faizanbhatt427@gmail.com>
2025-12-24 17:21:59 +0530

    SINGELTON.cpp

--- Design_Patterns/SINGELTON_DP/SINGELTON.cpp
+++ Design_Patterns/SINGELTON_DP/SINGELTON.cpp
@@ -1,9 +1,20 @@
 #include "integrate.hpp"
+void func_1()
+{
+	SINGELTON *ptr_1 = SINGELTON::create_instance(); 	
+	cout << "func_1() "<<ptr_1 << endl;
+}
+void func_2()
+{
+	SINGELTON *ptr_2 = SINGELTON::create_instance(); 
+	cout << "func_2() " << ptr_2 << endl;
+}
 int main()
 {
-SINGELTON *ptr_1 = SINGELTON::create_instance(); 
-SINGELTON *ptr_2 = SINGELTON::create_instance(); 
-
+	thread t1(func_1);
+	thread t2(func_2);
+	t1.join();
+	t2.join();
 return 0;
 }
 
