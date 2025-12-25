c7b91e7132272c354a5acc2bb43952a25e2b0b0c
faizan <faizanbhatt427@gmail.com>
2025-12-25 23:12:22 +0530

    main.cpp

--- Design_Patterns/BUILDER_DP/main.cpp
+++ Design_Patterns/BUILDER_DP/main.cpp
@@ -2,7 +2,12 @@
 #include "toy_box_builder.hpp"
 int main()
 {
-toy_box_builder Builder;
-toy_box *box_1 = Builder.add_car("AUDI")->add_doll("BARBIE")->add_stickers(true)->add_gift_wrap(true)->build();
+toy_box_builder Builder_1;
+toy_box *box_1 = Builder_1.add_car("AUDI")->add_doll("BARBIE")->add_stickers(true)->add_gift_wrap(true)->build();
+box_1->show();
+toy_box_builder Builder_2;
+toy_box *box_2 = Builder_2.add_car("AUDI")->add_doll("BARBIE")->add_gift_wrap(true)->build();
+box_2->show();
+
 return 0;
 }
