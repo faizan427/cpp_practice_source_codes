df97c149638503fcd66cc8b2f89810ba43402d2c
faizan <faizanbhatt427@gmail.com>
2025-12-26 23:26:29 +0530

    main.cpp

--- Design_Patterns/FACTORY_DP/main.cpp
+++ Design_Patterns/FACTORY_DP/main.cpp
@@ -5,5 +5,9 @@ toy_factory * car_makers = new car_factory();
 toy * audi_car = car_makers->make_toy();
 audi_car->play();
 
+toy_factory * doll_makers = new doll_factory();
+toy * barbie_doll = doll_makers->make_toy();
+barbie_doll->play();
+
 return 0;
 }
