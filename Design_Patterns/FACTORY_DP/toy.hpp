6021110b69ed122efd8146dc973e193c070884f2
faizan <faizanbhatt427@gmail.com>
2025-12-26 23:04:17 +0530

    toy.hpp

---
+++ Design_Patterns/FACTORY_DP/toy.hpp
@@ -0,0 +1,29 @@
+#ifndef TOY_HPP
+#define TOY_HPP
+#include <iostream>
+using namespace std;
+class toy
+{
+public:
+	toy()=default;
+	virtual void play() =0;
+};
+class car: public toy
+{
+public:
+	car() = default;
+	void play() override
+{
+	cout << "Play with CAR" << endl;
+}
+};
+class doll: public toy
+{
+public:	
+	doll = default;
+	void play() override
+{
+	cout << "Play with DOLL" << endl;
+}
+};
+#endif
