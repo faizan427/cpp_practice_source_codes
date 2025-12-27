f9b88186584282cf3e822720096869ff53e9dd5e
faizan <faizanbhatt427@gmail.com>
2025-12-27 23:30:07 +0530

    subject.hpp

--- Design_Patterns/OBSERVER_DP/subject.hpp
+++ Design_Patterns/OBSERVER_DP/subject.hpp
@@ -2,7 +2,26 @@
 class subject
 {
 public:
-	virtual void add_observer(observer &obs) = 0;
+	virtual void add_observer(observer &obser) = 0;
 	virtual void notify_observer() = 0;
 
+};
+
+class switch : public subject
+{
+public:
+	observer *obs;
+	void add_observer(observer *obser) override
+{
+	obs = obser;
+}
+	void notify_observer() override
+{
+	obs->update();
+}
+	void press_button
+{
+	cout << "SUBJECT: Button is pressed\n" << endl;
+	notify_observer();
+}
 };
