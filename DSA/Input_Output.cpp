3260f0a46f947b4a99953a8541df981ca63e229a
faizan <faizanbhatt427@gmail.com>
2026-01-02 22:51:00 +0530

    Input_Output.cpp

---
+++ DSA/Input_Output.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // Function to take input and display output 
+    void printNumber() {
+        int number;
+        
+        // Take input
+        cin >> number;
+        
+        // Print output
+        cout << number;
+    }    
+};
+
+// Driver code
+int main() {
+    // Creating an instance of Solution class
+    Solution sol;
+    
+    // Function call to take input and display output
+    sol.printNumber();
+    return 0;
+}
