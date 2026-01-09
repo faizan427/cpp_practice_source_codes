438a48fb0272d9640b1471fe927c6378f47ac4e7
faizan <faizanbhatt427@gmail.com>
2026-01-09 17:54:21 +0530

    na

---
+++ temp_practice/main.cpp
@@ -0,0 +1,16 @@
+#define i cin
+#define o cout
+#define PI 3.14
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int number;
+o << "enter number" << endl;
+i >> number;
+o << "number = " << number << " PI= " << PI << endl; 
+
+return 0;
+}
