6078b29ec5335347d427d85f0abdc3425b54194b
faizan <faizanbhatt427@gmail.com>
2026-01-09 16:06:23 +0530

    na

--- temp_practice/main.cpp
+++ temp_practice/main.cpp
@@ -1,36 +1,25 @@
 #include <iostream>
 using namespace std;
+void func(int a , int b)
+{
+if(b==0)
+{
+	throw 404;
+}
+else
+{
+	cout << "Division = "  << a/b << endl;
+}
+}
 int main()
 {
-int arr[10]={2,4,6,8,9,10,13,23,45,56};
-int l =0;
-int h = 9;
-int key = 10;
-
-int mid = 0;
-while(l<=h)
+try 
 {
-mid = (l+h)/2;
-
-if(arr[mid]>key)
-{
-	h = mid-1; 
+	func(8,0);
 }
-if(arr[mid] < key)
+catch(int e)
 {
-	l = mid +1;
-}
-if (arr[mid] ==  key)
-{
-
-cout << "Number found at index = "<< mid<<"\n" ;
-break;
-}
-else 
-{
-	cout << "Number not found\n";
-	
-}
+	cout << "Error " << e << "found" << endl;
 }
 
 return 0;
