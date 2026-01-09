79e4a8d687d5c4018e42c8424322eccd8e4e8c60
faizan <faizanbhatt427@gmail.com>
2026-01-09 16:24:36 +0530

    na

--- temp_practice/main.cpp
+++ temp_practice/main.cpp
@@ -1,25 +1,19 @@
 #include <iostream>
-#include <memory>
 using namespace std;
 class demo
 {
-public:	
-	demo()
+public:
+explicit	demo(int a)
 {
-	cout << "demo object created\n";
+cout << a << endl;
 }
-	~demo()
-{
-	cout << "destroyed\n";
-}
+
+
 };
 int main()
 {
-demo *d = new demo();
-cout << d << endl;
+demo D(2);
 
-unique_ptr<demo> D(std::move(d));
-	cout << d << endl;
 
 return 0;
 }
