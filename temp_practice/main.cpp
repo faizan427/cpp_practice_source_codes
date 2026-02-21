9132df9e6377d0ec57620ef8ced1ae97418f76e4
faizan <faizanbhatt427@gmail.com>
2026-02-21 19:28:21 +0530

    imag

--- temp_practice/main.cpp
+++ temp_practice/main.cpp
@@ -1,41 +1,49 @@
 #include <iostream>
-#include <thread>
-#include <mutex>
 using namespace std;
-class singelton
+class complex
 {
 private:
-static	singelton *ptr ;
-static mutex mtx;
-	singelton()
-{
-	cout << "instance created" << endl;
-} 
+	int real, imag;
 public:
-static singelton * create_instance();
-singelton(singelton &obj) = delete;
-singelton(singelton &&obj) noexcept = delete;
-singelton operator = (singelton &obj) = delete;
-};
-singelton * singelton::ptr = nullptr;
-mutex singelton::mtx;
-singelton *singelton::create_instance()
+	complex() = default;
+	complex(int real, int imag): real(real), imag(imag){}
+	complex  operator + (complex &obj)
 {
-	lock_guard<mutex> my_lock(mtx);	
-	if(ptr == nullptr)
-{
-	ptr = new singelton;
+	complex temp;
+	temp.real = this->real + obj.real;
+	temp.imag = this->imag + obj.imag;
+	return temp;
 }
-	return ptr;
+
+friend istream & operator >> (istream &i, complex &obj);
+
+friend void show(complex &num);
+
+
+};
+istream & operator >> (istream &i, complex &obj)
+{
+	cout << "enter real part " << endl;
+	cin >> obj.real;
+	cout << "enter imag part " << endl;
+	cin >> obj.imag;
+
+	return i;
+}
+
+void show(complex &num)
+{
+cout << num.real << " +i" << num.imag << endl;
 }
 int main()
 {
-auto func_1 = [=](){singelton *ins_1 =  singelton::create_instance();};
-auto func_2 = [=](){singelton *ins_2 =  singelton::create_instance();};
-thread t1(func_1);
-thread t2(func_2);
-t1.join();
-t2.join();
+complex num1(2,3);
+complex num2(3,2);
+complex num3 = num1+num2;
+show(num3);
+complex num4;
+cin>> num4;
+show(num4);
 
 return 0;
 }
